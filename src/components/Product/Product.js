6de181a58b64aeca46f423669e15887a03bed44d
Malgorzata Kaczmarczyk-Koza <kaczmarczyk.mmm@wp.pl>
2023-02-27 16:18:08 +0100

    Add useMemo to Product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import styles from './Product.module.scss';
 import PropTypes from 'prop-types';
 import ProductImage from '../ProductImage/ProductImage';
@@ -9,15 +9,15 @@ const Product = ({name, title, basePrice, colors, sizes}) => {
   const [currentSize, setCurrentSize] = useState(sizes[0].name);
   const [currentColor, setCurrentColor] = useState(colors[0]);
 
-  const getPrice = () => {
+  const price = useMemo(() => {
     const sizeSeleced = sizes.find(size => size.name === currentSize);
     return basePrice + sizeSeleced.additionalPrice;
-  }
+  }, [sizes, currentSize, basePrice]);
 
   const submitHandler = (event) => {
     event.preventDefault();
     console.log('Name ', title);
-    console.log('Price ', getPrice());
+    console.log('Price ', price);
     console.log('Size ', currentSize);
     console.log('Color ', currentColor);
   }
@@ -28,7 +28,7 @@ const Product = ({name, title, basePrice, colors, sizes}) => {
       <div>
         <header>
           <h2 className={styles.name}>{title}</h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <span className={styles.price}>Price: {price}$</span>
         </header>
         <ProductOption
           submitHandler={submitHandler}
